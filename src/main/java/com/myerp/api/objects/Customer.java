2fccf0207f9128afae34432a60ed1a4e61c6e471
dbaq <didier.baquier@myerp.com>
2014-06-13 11:34:52 -0700

    Update the customer object

--- src/main/java/com/myerp/api/objects/Customer.java
+++ src/main/java/com/myerp/api/objects/Customer.java
@@ -25,12 +25,53 @@ public class Customer extends MyERPObject {
   public Address shipping_address;
 
   @Field
-  public String first_name;
+  public String phone_number;
 
   @Field
-  public String last_name;
+  public String email;
 
   @Field
   public List<Contact> contacts;
+
+  @Field
+  public Integer payment_terms_id;
+
+  @Field
+  public String currency_iso_code;
+
+  @Field
+  public Integer sales_tax_id;
+
+  @Field
+  public String user_code;
+
+  @Field
+  public Boolean opt_in_to_emails;
+
+  @Field
+  public String comments;
+
+  //company only
+  @Field
+  public String company_name;
+
+  @Field
+  public Integer employee_count;
+
+  @Field
+  public String taxpayer_id_number;
+
+  @Field
+  public String siret;
+
+  //individual only
+  @Field
+  public String first_name;
+
+  @Field
+  public String middle_name;
+
+  @Field
+  public String last_name;
 
 }
